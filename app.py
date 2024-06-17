f0ee43fdebc31000ddc3197c9c868a36b3ccebbe
Thomas Weber <weber8thomas@gmail.com>
2024-06-17 18:06:36 +0200

    Update

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 import dash
-from dash import html
+from dash import html, dcc
 import dash_leaflet as dl
+from dash.dependencies import Input, Output
 
-# Initialize the Dash app
 app = dash.Dash(__name__)
 
-# Define the map
-map_center = [48.5734, 7.7521]  # Latitude and Longitude for Strasbourg
-zoom_level = 12  # Adjusted to show a close-up of the city
+# List of beacons with external website links
+beacons = [
+    {"name": "Beacon 1", "lat": 48.5765, "lon": 7.7480, "url": "https://www.google.com"},
+    {"name": "Beacon 2", "lat": 48.5805, "lon": 7.7500, "url": "https://www.amazon.com"}
+]
+
+def generate_marker(beacon):
+    return dl.Marker(position=[beacon['lat'], beacon['lon']], children=[
+        dl.Tooltip(beacon['name']),
+        dl.Popup([
+            html.A("Visit Site", href=beacon['url'], target="_blank")
+        ])
+    ])
 
 app.layout = html.Div([
-    dl.Map(center=map_center, zoom=zoom_level, style={'width': '100%', 'height': '90vh'}, children=[
-        dl.TileLayer()  # This will use the default OpenStreetMap tiles
-    ])
-], style={'height': '100vh'})  # Adjust height to fit the viewport
+    dl.Map(center=[48.5734, 7.7521], zoom=12, style={'width': '100%', 'height': '90vh'}, children=[
+        dl.TileLayer(),
+        dl.LayerGroup([generate_marker(beacon) for beacon in beacons])
+    ]),
+    html.Div(id="media-content", style={"margin": "20px"})
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
